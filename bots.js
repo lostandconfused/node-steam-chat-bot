3710c8007cc4494311ba77465898a50135b88129
Efreak <Efreak@users.noreply.github.com>
2015-08-28 15:15:53 -0700

    mate timeout is 10s, avoid loops

--- bots.js
+++ bots.js
@@ -302,7 +302,7 @@ miley.addTriggers([
 	{ name: 'SteamIDCheck',    type: 'SteamrepTrigger',        options: { command: "!steamrep", delay: 0, timeout: 2*1000 } },
 	{ name: 'SingleUserReply', type: 'ChatReplyTrigger',       options: { matches: ['hi bot'], responses: ['hi boss!','hi master!'], exact: true, user: users.efreak,	users: [users.efreak] } },
 //	{ name: 'IsUp',            type: 'isupTrigger',            options: { command: '!isup' } },
-	{ name: 'MateEscal',       type: 'RegexReplaceTrigger',    options: { match: /^(m+?)(a+?)te(s??)$/, response: '{0}m{1}aaate{2}', delay: 500, ignore: [rooms.BLG,rooms.rg,rooms.OPS,rooms.OPT,rooms.OPG] } },
+	{ name: 'MateEscal',       type: 'RegexReplaceTrigger',    options: { match: /^(m+?)(a+?)te(s??)$/, response: '{0}m{1}aaate{2}', delay: 500, ignore: [rooms.BLG,rooms.rg,rooms.OPS,rooms.OPT,rooms.OPG], timeout: 10*1000 } },
 //	{ name: 'ButtBot',         type: 'ButtBotTrigger',         options: { replacement: 'butt', probability: 0.008, delay: 1000, timeout: 2*60*60*1000, ignore: [rooms.BLG,rooms.OPS,rooms.OPT,rooms.OPG] } },
 //	{ name: 'ButtReply',       type: 'ChatReplyTrigger',       options: { matches: ['butt'], responses: ['lol, you said butt'], probability: 0.5, timeout: 2*60*60*1000, delay: 500, ignore: [rooms.BLG,rooms.OPS,rooms.OPT,rooms.OPG] } },
 	{ name: 'PenisReply',      type: 'ChatReplyTrigger',       options: { matches: ['penis'], responses: ['lol, you said penis'], probability: 0.15, timeout: 2*60*60*1000, delay: 500, ignore: [rooms.BLG,rooms.OPS,rooms.OPT,rooms.OPG]	} }, //someone wanted this...
